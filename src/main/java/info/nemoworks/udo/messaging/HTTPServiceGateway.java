71ca3654080a1445f1c14588cc50b17d70e40242
NovelistChan <774527381@qq.com>
2021-05-21 14:53:15 +0800

    update udoevent

--- src/main/java/info/nemoworks/udo/messaging/HTTPServiceGateway.java
+++ src/main/java/info/nemoworks/udo/messaging/HTTPServiceGateway.java
@@ -1,9 +1,11 @@
 package info.nemoworks.udo.messaging;
 
 import com.google.common.eventbus.Subscribe;
+import info.nemoworks.udo.model.EventType;
 import info.nemoworks.udo.model.Udo;
 import info.nemoworks.udo.model.UdoEvent;
 import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,84 +26,105 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class HTTPServiceGateway extends UdoGateway {
 
-        private final HttpClient client;
+    private final HttpClient client;
 
-        public Map<String, URI> getEndpoints() {
-                return endpoints;
+    public Map<String, URI> getEndpoints() {
+        return endpoints;
+    }
+
+    private final Map<String, URI> endpoints;
+
+    public HTTPServiceGateway() {
+        super();
+        endpoints = new HashMap<>();
+
+        client = HttpClient.newBuilder().version(Version.HTTP_1_1).followRedirects(Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20)).build();
+    }
+
+    @Subscribe
+    public void udoEvent(UdoEvent udoEvent) {
+        try {
+            Udo udo = (Udo) udoEvent.getSource();
+            EventType contextId = udoEvent.getContextId();
+            switch (contextId) {
+                case SAVE:
+                    this.register(udo.getId(), new URI(udo.uri));
+                    break;
+                case SYNC:
+                case UPDATE:
+                    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(new String(udoEvent.getPayload())))
+                            .header("Content-Type", "application/json").GET().timeout(Duration.ofMinutes(2))
+                            .build();
+                    HttpResponse<String> body = client.send(request, BodyHandlers.ofString());
+                    this.updateUdo(udo.getId(), body.body().getBytes());
+                    break;
+                case DELETE:
+                    this.unregister(udo.getId(), new URI(udo.uri));
+                default:
+                    this.register(udo.getId(), new URI(udo.uri));
+            }
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            e.printStackTrace();
         }
+        System.out.println(udoEvent.toString());
+    }
 
-        private final Map<String, URI> endpoints;
 
-        public HTTPServiceGateway() {
-                super();
-                endpoints = new HashMap<>();
+    public void register(String tag, URI uri) {
+        endpoints.put(tag, uri);
+    }
 
-                client = HttpClient.newBuilder().version(Version.HTTP_1_1).followRedirects(Redirect.NORMAL)
-                                .connectTimeout(Duration.ofSeconds(20)).build();
-        }
+    public void unregister(String tag, URI uri) {
+        endpoints.remove(tag, uri);
+    }
 
-        @Subscribe
-        public void udoEvent(UdoEvent udoEvent){
-                try {
-                        Udo udo = (Udo) udoEvent.getSource();
-                        this.register(udo.getId(),new URI(udo.uri));
-                } catch (URISyntaxException e) {
+    public void start() throws InterruptedException {
+
+        CountDownLatch receivedSignal = new CountDownLatch(10);
+
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(() -> {
+            try {
+
+                System.out.println("fetching data...");
+                this.endpoints.entrySet().forEach(entry -> {
+                    try {
+                        downlink(entry.getKey(), entry.getValue().toString().getBytes());
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
                         e.printStackTrace();
-                }
-                System.out.println(udoEvent.toString());
-        }
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
 
+                });
+                receivedSignal.countDown();
 
-        public void register(String tag, URI uri) {
-                endpoints.put(tag, uri);
-        }
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }, 1, 1, TimeUnit.SECONDS);
 
-        public void start() throws InterruptedException {
+        receivedSignal.await(1, TimeUnit.MINUTES);
 
-                CountDownLatch receivedSignal = new CountDownLatch(10);
+        executor.shutdown();
 
-                ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-                executor.scheduleAtFixedRate(() -> {
-                        try {
+    }
 
-                                System.out.println("fetching data...");
-                                this.endpoints.entrySet().forEach(entry -> {
-                                        try {
-                                                downlink(entry.getKey(), entry.getValue().toString().getBytes());
-                                        } catch (IOException e) {
-                                                // TODO Auto-generated catch block
-                                                e.printStackTrace();
-                                        } catch (InterruptedException e) {
-                                                // TODO Auto-generated catch block
-                                                e.printStackTrace();
-                                        }
+    @Override
+    public void downlink(String tag, byte[] payload) throws IOException, InterruptedException {
 
-                                });
-                                receivedSignal.countDown();
-
-                        } catch (Exception ex) {
-                                throw new RuntimeException(ex);
-                        }
-                }, 1, 1, TimeUnit.SECONDS);
-
-                receivedSignal.await(1, TimeUnit.MINUTES);
-
-                executor.shutdown();
-
-        }
-
-        @Override
-        public void downlink(String tag, byte[] payload) throws IOException, InterruptedException {
-
-                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(new String(payload)))
-                                .header("Content-Type", "application/json").GET().timeout(Duration.ofMinutes(2))
-                                .build();
-                HttpResponse<String> body = client.send(request,BodyHandlers.ofString());
-                System.out.println(body.body());
-                this.updateUdo(tag,body.body().getBytes());
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(new String(payload)))
+                .header("Content-Type", "application/json").GET().timeout(Duration.ofMinutes(2))
+                .build();
+        HttpResponse<String> body = client.send(request, BodyHandlers.ofString());
+        System.out.println(body.body());
+        this.updateUdo(tag, body.body().getBytes());
 //                client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body)
 //                                .thenAccept(body -> this.updateUdo(tag, body.getBytes()));
-        }
+    }
 
 
 }
--- src/main/java/info/nemoworks/udo/messaging/UdoGateway.java
+++ src/main/java/info/nemoworks/udo/messaging/UdoGateway.java
@@ -3,20 +3,23 @@ package info.nemoworks.udo.messaging;
 import com.google.common.eventbus.EventBus;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import info.nemoworks.udo.model.SyncEvent;
+import info.nemoworks.udo.model.EventType;
 import info.nemoworks.udo.model.Udo;
+import info.nemoworks.udo.model.UdoEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 
+//import info.nemoworks.udo.model.SyncEvent;
+
 public abstract class UdoGateway {
 
-//    protected MessagingManager messagingManager;
+    //    protected MessagingManager messagingManager;
     @Autowired
     EventBus eventBus;
 
     public enum UdoGatewayType {
-        HTTP,MQTT
+        HTTP, MQTT
     }
 
     public UdoGatewayType getType() {
@@ -41,12 +44,12 @@ public abstract class UdoGateway {
 //    }
 
     //upadte udo
-    protected void updateUdo(String tag,byte[] payload){
+    protected void updateUdo(String tag, byte[] payload) {
         String s = "{'Name':'Jeep'}";
-        JsonObject data = new Gson().fromJson(new String(payload),JsonObject.class);
-        Udo udo = new Udo(null,data);
+        JsonObject data = new Gson().fromJson(new String(payload), JsonObject.class);
+        Udo udo = new Udo(null, data);
         udo.setId(tag);
-        eventBus.post(new SyncEvent("sync",udo));
+        eventBus.post(new UdoEvent(EventType.SYNC, udo, payload));
 //        Udo udo = udoService.getUdoById(tag);
 //        udo.setData(data);
 //        try {
